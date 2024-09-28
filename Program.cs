2ecc93b382d8a18ee04f3d377f64611eef98baeb
THINH_NGUYEN\thinh <thinhnguyen190206@gmail.com>
2024-09-28 20:36:37 +0700

    2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,18 @@ namespace learn_2
     {
         static void Main(string[] args)
         {
+            int a = 5;
+            a -= 1;
+            Console.WriteLine(a);
+            int b = 5;
+            b *= 3;
+            Console.WriteLine(b);
+            Console.WriteLine(a == b);
+            bool Logic1 = a != b;
+            bool Logic2 = (a %= 2) == 0;
+            Console.WriteLine(Logic1 && Logic2 );
+            Console.WriteLine(b != a);
+            Console.ReadLine();
         }
     }
 }
